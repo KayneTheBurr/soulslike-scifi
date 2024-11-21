d2dbe075fd74de04b2b87598e8312f0c6f14e6ae
KayneTheBurr <kainejohnson63@gmail.com>
2024-11-21 00:23:34 -0800

    Adding first enemy

---
+++ Combat System/Assets/2. Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "Scriptable Objects/Enemy")]
+public class Enemy : ScriptableObject
+{
+    public float health, attack, defense;
+
+    public void Attack()
+    {
+
+    }
+}
